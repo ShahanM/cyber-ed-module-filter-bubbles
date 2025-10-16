c226c63747018fc2006afee417e4e34fa5575258
Mehtab "Shahan" Iqbal <mehtabi@clemson.edu>
2025-10-15 23:56:32 -0400

    forgot something again

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react(), tailwindcss()],
-	base: "/filter-bubbles-module/",
+	base: "/cybered/filter-bubbles-module/",
 	build: {
 		outDir: "build",
 	},
